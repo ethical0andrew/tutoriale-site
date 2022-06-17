240ed152da67418796d71adf1ad3b761d66b9e85
Andrew <frintu.andrei07@gmail.com>
2022-06-17 23:03:51 +0300

    Update index.php

--- php/crud-php-mysql/index.php
+++ php/crud-php-mysql/index.php
@@ -1,5 +1,8 @@
 <?php
-
+    $conn = mysqli_connect('localhost', 'root', '', 'crud');
+    if (!$conn) { echo "EROARE LA CONECTAREA BAZEI DE DATE"; }
+    $sql = "SELECT * FROM `utilizatori`";
+    $query = mysqli_query($conn, $sql);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,6 +13,10 @@
     <title>Aplicatie CRUD</title>
 </head>
 <body>
-    <a href="creare.php">Creare utilizator</a>
+    <a href="creare.php">Creare utilizator</a><br>
+    <?php while ($i = mysqli_fetch_assoc($query)) { ?>
+    <a href="citire.php?id=<?php echo $i['id'];?>">
+    Utilizatorul <?php echo $i['id'];?></a><br>
+    <?php } ?>
 </body>
 </html>
